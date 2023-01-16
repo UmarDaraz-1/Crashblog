bf9a257bd4a2c773b1f3e69f1389abe9804ef03b
Umar Daraz <umardaraz03027692357u@gmail.com>
2023-01-16 23:42:08 +0500

    first

---
+++ crashblog/core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, HttpResponse
+from blog.models import Post
+# Create your views here.
+def frontpage(request):
+    posts = Post.objects.filter(status=Post.ACTIVE)
+    return render(request, 'core/frontpage.html', {"posts": posts})
+
+def about(request):
+    return render(request, 'core/about.html')
+
+
+def robots_txt(request):
+    text = [
+        "User-Agent: *",
+        "Disallow: /admin/",
+    ]
+    return HttpResponse("\n".join(text), content_type="text/plain")
